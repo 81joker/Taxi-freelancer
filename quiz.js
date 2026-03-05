fbe36d7eacacb25955a21f9cbc1e75fdf36935fc
Nehad <35563834+81joker@users.noreply.github.com>
2026-03-05 07:51:36 +0100

    Add location-based questions to the quiz

--- quiz.js
+++ quiz.js
@@ -241,6 +241,261 @@ const questions = [
         answer: "Schmerlingplatz 10-11"
     },
 
+
+
+
+    {
+        question: "Wo ist das Landesgericht für Strafsachen?",
+        options: [
+            "Landesgerichtsstraße 11",
+            "Wickenburggasse 18-20",
+            "Schottenring 7-9",
+            "Dietrichgasse 27"
+        ],
+        answer: "Landesgerichtsstraße 11"
+    },
+
+    {
+        question: "Wo ist die Justizanstalt Wien Josefstadt?",
+        options: [
+            "Wickenburggasse 18-20",
+            "Landesgerichtsstraße 11",
+            "Althanstraße 39-45",
+            "Dampfschiffstraße 2"
+        ],
+        answer: "Wickenburggasse 18-20"
+    },
+
+    {
+        question: "Wo ist das Justiz-Zentrum Wien Mitte?",
+        options: [
+            "Marxergasse 1a",
+            "Landesgerichtsstraße 11",
+            "Wickenburggasse 18-20",
+            "Althanstraße 39-45"
+        ],
+        answer: "Marxergasse 1a"
+    },
+
+    {
+        question: "Wo ist das Arbeits- und Sozialgericht Wien?",
+        options: [
+            "Althanstraße 39-45",
+            "Marxergasse 1a",
+            "Wickenburggasse 18-20",
+            "Landesgerichtsstraße 11"
+        ],
+        answer: "Althanstraße 39-45"
+    },
+
+    {
+        question: "Wo ist das Landesgerichtliches Gefangenenhaus II?",
+        options: [
+            "Hernalser Gürtel 6-12",
+            "Althanstraße 39-45",
+            "Wickenburggasse 18-20",
+            "Marxergasse 1a"
+        ],
+        answer: "Hernalser Gürtel 6-12"
+    },
+
+    {
+        question: "Wo ist der Rechnungshof?",
+        options: [
+            "Dampfschiffstraße 2",
+            "Marxergasse 1a",
+            "Schottenring 7-9",
+            "Althanstraße 39-45"
+        ],
+        answer: "Dampfschiffstraße 2"
+    },
+
+    {
+        question: "Wo ist die Bundespolizeidirektion Wien?",
+        options: [
+            "Schottenring 7-9",
+            "Hernalser Gürtel 6-12",
+            "Dietrichgasse 27",
+            "Deutschmeisterplatz 3"
+        ],
+        answer: "Schottenring 7-9"
+    },
+
+    {
+        question: "Wo ist das Fremdenpolizeiliche Büro?",
+        options: [
+            "Hernalser Gürtel 6-12",
+            "Schottenring 7-9",
+            "Dietrichgasse 27",
+            "Deutschmeisterplatz 3"
+        ],
+        answer: "Hernalser Gürtel 6-12"
+    },
+
+    {
+        question: "Wo ist das Verkehrsamt (Führerschein / Taxireferat)?",
+        options: [
+            "Dietrichgasse 27",
+            "Hernalser Gürtel 6-12",
+            "Schottenring 7-9",
+            "Deutschmeisterplatz 3"
+        ],
+        answer: "Dietrichgasse 27"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Innere Stadt?",
+        options: [
+            "Deutschmeisterplatz 3",
+            "Juchgasse 19",
+            "Viktor-Christ-Gasse 19",
+            "Fuhrmannsgasse 5"
+        ],
+        answer: "Deutschmeisterplatz 3"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Landstraße?",
+        options: [
+            "Juchgasse 19",
+            "Deutschmeisterplatz 3",
+            "Viktor-Christ-Gasse 19",
+            "Fuhrmannsgasse 5"
+        ],
+        answer: "Juchgasse 19"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Margareten (Bezirke 4,5,6)?",
+        options: [
+            "Viktor-Christ-Gasse 19",
+            "Juchgasse 19",
+            "Fuhrmannsgasse 5",
+            "Van-der-Nüll-Gasse 11"
+        ],
+        answer: "Viktor-Christ-Gasse 19"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Josefstadt (Bezirke 7,8,9)?",
+        options: [
+            "Fuhrmannsgasse 5",
+            "Viktor-Christ-Gasse 19",
+            "Van-der-Nüll-Gasse 11",
+            "Enkplatz 3"
+        ],
+        answer: "Fuhrmannsgasse 5"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Favoriten?",
+        options: [
+            "Van-der-Nüll-Gasse 11",
+            "Fuhrmannsgasse 5",
+            "Enkplatz 3",
+            "Hohenbergstraße 1"
+        ],
+        answer: "Van-der-Nüll-Gasse 11"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Simmering?",
+        options: [
+            "Enkplatz 3",
+            "Van-der-Nüll-Gasse 11",
+            "Hohenbergstraße 1",
+            "Tannengasse 6"
+        ],
+        answer: "Enkplatz 3"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Meidling?",
+        options: [
+            "Hohenbergstraße 1",
+            "Enkplatz 3",
+            "Tannengasse 6",
+            "Wattgasse 15"
+        ],
+        answer: "Hohenbergstraße 1"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Fünfhaus?",
+        options: [
+            "Tannengasse 6",
+            "Hohenbergstraße 1",
+            "Wattgasse 15",
+            "Hohe Warte 32"
+        ],
+        answer: "Tannengasse 6"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Ottakring?",
+        options: [
+            "Wattgasse 15",
+            "Tannengasse 6",
+            "Hohe Warte 32",
+            "Pappenheimgasse 33"
+        ],
+        answer: "Wattgasse 15"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Döbling?",
+        options: [
+            "Hohe Warte 32",
+            "Wattgasse 15",
+            "Pappenheimgasse 33",
+            "Hermann Bahr Straße 3"
+        ],
+        answer: "Hohe Warte 32"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Brigittenau?",
+        options: [
+            "Pappenheimgasse 33",
+            "Hohe Warte 32",
+            "Hermann Bahr Straße 3",
+            "Wagramer Straße 89"
+        ],
+        answer: "Pappenheimgasse 33"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Floridsdorf?",
+        options: [
+            "Hermann Bahr Straße 3",
+            "Pappenheimgasse 33",
+            "Wagramer Straße 89",
+            "Lehmanngasse 3a"
+        ],
+        answer: "Hermann Bahr Straße 3"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Donaustadt?",
+        options: [
+            "Wagramer Straße 89",
+            "Hermann Bahr Straße 3",
+            "Lehmanngasse 3a",
+            "Pappenheimgasse 33"
+        ],
+        answer: "Wagramer Straße 89"
+    },
+
+    {
+        question: "Wo ist die Polizeiinspektion Liesing?",
+        options: [
+            "Lehmanngasse 3a",
+            "Wagramer Straße 89",
+            "Hermann Bahr Straße 3",
+            "Pappenheimgasse 33"
+        ],
+        answer: "Lehmanngasse 3a"
+    },
 ];
 
 // عدد الأسئلة الكلي
