264c0b961e8339376662f6ce1a8052cacef30820
joker81 <nehad.al.timimi@gmail.com>
2026-06-06 18:14:04 +0200

    Update Magistratische Bezirksämter questions to include district numbers for clarity

--- js/Magistratische&Bezirksämter.js
+++ js/Magistratische&Bezirksämter.js
@@ -3,222 +3,223 @@ const questions = [
     {
         question: "Wo ist das Magistratische Bezirksamt für den 1. und 8. Bezirk?",
         options: [
-            "Wipplingerstraße 8",
-            "Karmelitergasse 9",
-            "Karl-Borromäus-Platz 3",
-            "Rechte Wienzeile 105"
+            "1. Wipplingerstraße 8",
+            "2. Karmelitergasse 9",
+            "3. Karl-Borromäus-Platz 3",
+            "5. Rechte Wienzeile 105"
         ],
-        answer: "Wipplingerstraße 8"
+        answer: "1. Wipplingerstraße 8"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 2. Bezirk?",
         options: [
-            "Karmelitergasse 9",
-            "Wipplingerstraße 8",
-            "Karl-Borromäus-Platz 3",
-            "Rechte Wienzeile 105"
+            "2. Karmelitergasse 9",
+            "1. Wipplingerstraße 8",
+            "3. Karl-Borromäus-Platz 3",
+            "5. Rechte Wienzeile 105"
         ],
-        answer: "Karmelitergasse 9"
+        answer: "2. Karmelitergasse 9"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 3. Bezirk?",
         options: [
-            "Karl-Borromäus-Platz 3",
-            "Karmelitergasse 9",
-            "Wipplingerstraße 8",
-            "Rechte Wienzeile 105"
+            "3. Karl-Borromäus-Platz 3",
+            "2. Karmelitergasse 9",
+            "1. Wipplingerstraße 8",
+            "5. Rechte Wienzeile 105"
         ],
-        answer: "Karl-Borromäus-Platz 3"
+        answer: "3. Karl-Borromäus-Platz 3"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 4. und 5. Bezirk?",
         options: [
-            "Rechte Wienzeile 105",
-            "Hermanngasse 24-26",
-            "Wilhelm Exner-Gasse 5",
-            "Laxenburger Straße 43-45"
+            "5. Rechte Wienzeile 105",
+            "7. Hermanngasse 24-26",
+            "9. Wilhelm Exner-Gasse 5",
+            "10. Laxenburger Straße 43-45"
         ],
-        answer: "Rechte Wienzeile 105"
+        answer: "5. Rechte Wienzeile 105"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 6. und 7. Bezirk?",
         options: [
-            "Hermanngasse 24-26",
-            "Wilhelm Exner-Gasse 5",
-            "Laxenburger Straße 43-45",
-            "Enkplatz 2"
+            "7. Hermanngasse 24-26",
+            "9. Wilhelm Exner-Gasse 5",
+            "10. Laxenburger Straße 43-45",
+            "11. Enkplatz 2"
         ],
-        answer: "Hermanngasse 24-26"
+        answer: "7. Hermanngasse 24-26"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 9. Bezirk?",
         options: [
-            "Wilhelm Exner-Gasse 5",
-            "Hermanngasse 24-26",
-            "Laxenburger Straße 43-45",
-            "Enkplatz 2"
+            "9. Wilhelm Exner-Gasse 5",
+            "7. Hermanngasse 24-26",
+            "10. Laxenburger Straße 43-45",
+            "11. Enkplatz 2"
         ],
-        answer: "Wilhelm Exner-Gasse 5"
+        answer: "9. Wilhelm Exner-Gasse 5"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 10. Bezirk?",
         options: [
-            "Laxenburger Straße 43-45",
-            "Wilhelm Exner-Gasse 5",
-            "Hermanngasse 24-26",
-            "Enkplatz 2"
+            "10. Laxenburger Straße 43-45",
+            "9. Wilhelm Exner-Gasse 5",
+            "7. Hermanngasse 24-26",
+            "11. Enkplatz 2"
         ],
-        answer: "Laxenburger Straße 43-45"
+        answer: "10. Laxenburger Straße 43-45"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 11. Bezirk?",
         options: [
-            "Enkplatz 2",
-            "Laxenburger Straße 43-45",
-            "Wilhelm Exner-Gasse 5",
-            "Hermanngasse 24-26"
+            "11. Enkplatz 2",
+            "10. Laxenburger Straße 43-45",
+            "9. Wilhelm Exner-Gasse 5",
+            "7. Hermanngasse 24-26"
         ],
-        answer: "Enkplatz 2"
+        answer: "11. Enkplatz 2"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 12. Bezirk?",
         options: [
-            "Schönbrunner Straße 259",
-            "Enkplatz 2",
-            "Hermanngasse 24-26",
-            "Laxenburger Straße 43-45"
+            "12. Schönbrunner Straße 259",
+            "11. Enkplatz 2",
+            "7. Hermanngasse 24-26",
+            "10. Laxenburger Straße 43-45"
         ],
-        answer: "Schönbrunner Straße 259"
+        answer: "12. Schönbrunner Straße 259"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 13. und 14. Bezirk?",
         options: [
-            "Hietzinger Kai 1-3",
-            "Schönbrunner Straße 259",
-            "Gasgasse 8-10",
-            "Richard-Wagner-Platz 19"
+            "13. Hietzinger Kai 1-3",
+            "12. Schönbrunner Straße 259",
+            "15. Gasgasse 8-10",
+            "16. Richard-Wagner-Platz 19"
         ],
-        answer: "Hietzinger Kai 1-3"
+        answer: "13. Hietzinger Kai 1-3"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 15. Bezirk?",
         options: [
-            "Gasgasse 8-10",
-            "Hietzinger Kai 1-3",
-            "Richard-Wagner-Platz 19",
-            "Elterleinplatz 14"
+            "15. Gasgasse 8-10",
+            "13. Hietzinger Kai 1-3",
+            "16. Richard-Wagner-Platz 19",
+            "17. Elterleinplatz 14"
         ],
-        answer: "Gasgasse 8-10"
+        answer: "15. Gasgasse 8-10"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 16. Bezirk?",
         options: [
-            "Richard-Wagner-Platz 19",
-            "Gasgasse 8-10",
-            "Elterleinplatz 14",
-            "Martinstraße 100"
+            "16. Richard-Wagner-Platz 19",
+            "15. Gasgasse 8-10",
+            "17. Elterleinplatz 14",
+            "18. Martinstraße 100"
         ],
-        answer: "Richard-Wagner-Platz 19"
+        answer: "16. Richard-Wagner-Platz 19"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 17. Bezirk?",
         options: [
-            "Elterleinplatz 14",
-            "Richard-Wagner-Platz 19",
-            "Martinstraße 100",
-            "Pfarrwiesengasse 23c"
+            "17. Elterleinplatz 14",
+            "16. Richard-Wagner-Platz 19",
+            "18. Martinstraße 100",
+            "19. Pfarrwiesengasse 23c"
         ],
-        answer: "Elterleinplatz 14"
+        answer: "17. Elterleinplatz 14"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 18. Bezirk?",
         options: [
-            "Martinstraße 100",
-            "Elterleinplatz 14",
-            "Pfarrwiesengasse 23c",
-            "Brigittaplatz 10"
+            "18. Martinstraße 100",
+            "17. Elterleinplatz 14",
+            "19. Pfarrwiesengasse 23c",
+            "20. Brigittaplatz 10"
         ],
-        answer: "Martinstraße 100"
+        answer: "18. Martinstraße 100"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 19. Bezirk?",
         options: [
-            "Pfarrwiesengasse 23c",
-            "Martinstraße 100",
-            "Brigittaplatz 10",
-            "Am Spitz 1"
+            "19. Pfarrwiesengasse 23c",
+            "18. Martinstraße 100",
+            "20. Brigittaplatz 10",
+            "21. Am Spitz 1"
         ],
-        answer: "Pfarrwiesengasse 23c"
+        answer: "19. Pfarrwiesengasse 23c"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 20. Bezirk?",
         options: [
-            "Brigittaplatz 10",
-            "Pfarrwiesengasse 23c",
-            "Am Spitz 1",
-            "Schrödingerplatz 1"
+            "20. Brigittaplatz 10",
+            "19. Pfarrwiesengasse 23c",
+            "21. Am Spitz 1",
+            "22. Schrödingerplatz 1"
         ],
-        answer: "Brigittaplatz 10"
+        answer: "20. Brigittaplatz 10"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 21. Bezirk?",
         options: [
-            "Am Spitz 1",
-            "Brigittaplatz 10",
-            "Schrödingerplatz 1",
-            "Perchtoldsdorfer Straße 2"
+            "21. Am Spitz 1",
+            "20. Brigittaplatz 10",
+            "22. Schrödingerplatz 1",
+            "23. Perchtoldsdorfer Straße 2"
         ],
-        answer: "Am Spitz 1"
+        answer: "21. Am Spitz 1"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 22. Bezirk?",
         options: [
-            "Schrödingerplatz 1",
-            "Am Spitz 1",
-            "Brigittaplatz 10",
-            "Perchtoldsdorfer Straße 2"
+            "22. Schrödingerplatz 1",
+            "21. Am Spitz 1",
+            "20. Brigittaplatz 10",
+            "23. Perchtoldsdorfer Straße 2"
         ],
-        answer: "Schrödingerplatz 1"
+        answer: "22. Schrödingerplatz 1"
     },
 
     {
         question: "Wo ist das Magistratische Bezirksamt für den 23. Bezirk?",
         options: [
-            "Perchtoldsdorfer Straße 2",
-            "Schrödingerplatz 1",
-            "Am Spitz 1",
-            "Brigittaplatz 10"
+            "23. Perchtoldsdorfer Straße 2",
+            "22. Schrödingerplatz 1",
+            "21. Am Spitz 1",
+            "20. Brigittaplatz 10"
         ],
-        answer: "Perchtoldsdorfer Straße 2"
+        answer: "23. Perchtoldsdorfer Straße 2"
     },
 
     {
         question: "Wo ist das Fundservice Wien?",
         options: [
-            "Siebenbrunnenfeldgasse 3",
-            "Gasgasse 8-10",
-            "Brigittaplatz 10",
-            "Am Spitz 1"
+            "5. Siebenbrunnenfeldgasse 3",
+            "15. Gasgasse 8-10",
+            "20. Brigittaplatz 10",
+            "21. Am Spitz 1"
         ],
-        answer: "Siebenbrunnenfeldgasse 3"
+        answer: "5. Siebenbrunnenfeldgasse 3"
     },
+
 ];
 // عدد الأسئلة الكلي
 const totalQuestions = questions.length;
